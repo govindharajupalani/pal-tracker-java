1f53176ba5521aa56133ade5c1d0cbd9f3198358
govindharaju Palani <govindharaju.palani@gmail.com>
2019-07-22 08:59:27 +0530

    Config

---
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -0,0 +1,48 @@
+package io.pivotal.pal.tracker;
+
+import com.fasterxml.jackson.databind.deser.std.StringArrayDeserializer;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class EnvController
+{
+
+    private String m_Port;
+    private String m_MemLimit;
+    private String m_Index;
+    private String m_Address;
+
+    public EnvController(@Value("${PORT:port_not_set}") String m_Port,
+                         @Value("${memory.limit:mem_Not set}") String m_MemLimit,
+                         @Value("${cf.instance.index:index_not_set}") String m_Index,
+                         @Value("${cf.instance.addr:address_not_set}")String m_Address) {
+        this.m_Port = m_Port;
+        this.m_MemLimit = m_MemLimit;
+        this.m_Index = m_Index;
+        this.m_Address = m_Address;
+    }
+
+    @GetMapping("/env")
+    public Map<String,String>  GetEnv()
+    {
+        Map<String , String > env = new HashMap();
+        env.put("PORT",m_Port );
+        env.put("MEMORY",m_MemLimit );
+        env.put("CF_INSTANCE_INDEX",m_Index );
+        env.put("CF_INSTANCE_ADDR",m_Address );
+
+        return env;
+    }
+
+
+
+
+
+
+}
--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,26 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    private String welcome_message;
+
+    @Autowired
+    private Environment env;
+
+    WelcomeController(@Value("${welcome.message}") String welmsg )
+    {
+        welcome_message = welmsg;
+    }
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return  env.getProperty("WELCOME_MESSAGE");
     }
 }
